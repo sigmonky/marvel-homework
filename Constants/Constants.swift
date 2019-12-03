b1a55b4c3b24a13d8f95f4e13daa7734184fb9a8
Randy Weinstein <sigmonky@gmail.com>
2019-12-03 16:46:06 -0500

    took out api keys

--- Constants/Constants.swift
+++ Constants/Constants.swift
@@ -3,8 +3,8 @@
 import Foundation
 
 struct APIConstants {
-  static let publicKey =  "putyourshere"
-  static let privateKey = "putyourshere"
+  static let publicKey =  "put yours here"
+  static let privateKey = "put yours here"
   static let baseEndPoinUrl = URL(string: "https://gateway.marvel.com:443/v1/public/")!
   static let basePointErrorMsg = "Could not resolve API endpoint"
 }
